9fd28e7c2e4cb17edf685b5e1e851e86c9ebf681
Volodymyr Yavdoshenko <v.yavdoshenko@gmail.com>
2024-05-03 23:08:57 +0300

    command line parser has been fixed

--- life/src/main.rs
+++ life/src/main.rs
@@ -1,18 +1,26 @@
-use clap::{Command, arg, crate_version, crate_authors, crate_description};
+use clap::{arg, crate_authors, crate_description, crate_version, value_parser, Command};
 
 fn main() {
     let matches = Command::new("The Game of Life")
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .arg(arg!(-r --rows "Sets the number of rows")
-             .required(true))
-        .arg(arg!(-c --columns "Sets the number of columns")
-             .required(true))
+        .arg(
+            arg!(-r --rows <VALUE> "Sets the number of rows")
+                .required(true)
+                .value_parser(value_parser!(u16)),
+        )
+        .arg(
+            arg!(-c --columns <VALUE> "Sets the number of columns")
+                .required(true)
+                .value_parser(value_parser!(u16)),
+        )
         .get_matches();
 
-    let rows = matches.get_one::<String>("rows").expect("required argument");
-    let columns = matches.get_one::<String>("columns").expect("required argument");
+    let rows = matches.get_one::<u16>("rows").expect("required argument");
+    let columns = matches
+        .get_one::<u16>("columns")
+        .expect("required argument");
 
     println!("Number of rows: {}", rows);
     println!("Number of columns: {}", columns);
